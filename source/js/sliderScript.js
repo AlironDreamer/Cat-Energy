4061fafc71e10101246fab89dd9e0d0fccafc352
Aliron <a.taraskin02@gmail.com>
2024-05-19 15:57:34 +0300

    Финальная верстка перед автоматизацией

--- source/js/sliderScript.js
+++ source/js/sliderScript.js
@@ -7,19 +7,24 @@ var afterBtn = document.querySelector('#after-button');
 
 range.addEventListener('input',
   function() {
+    beforeContainer.style.clipPath = "polygon(0% 0%, 0% 100%, " + (100-this.value) +"% 100%, " + (100-this.value) +"% 0%)";
     afterContainer.style.width = this.value + "%";
   }, false);
 
 beforeBtn.addEventListener('click',
   function() {
+    beforeContainer.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
     afterContainer.style.width = "0%";
     togglerMarker.style.left = "6px"
+    togglerMarker.style.right = "50%";
     range.value = 1;
   }, false);
 
 afterBtn.addEventListener('click',
   function() {
+    beforeContainer.style.clipPath = "polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)";
     afterContainer.style.width = "100%";
-    togglerMarker.style.left = "44px"
+    togglerMarker.style.right = "6px";
+    togglerMarker.style.left = "50%";
     range.value = 100;
   }, false)
