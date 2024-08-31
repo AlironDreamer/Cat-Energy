b9583f8f590c411a1fff0238b0ce1eeae5b0bc50
Aliron <a.taraskin02@gmail.com>
2024-08-31 11:18:13 +0300

    Исправлено расположение блоков программ при ширине 1340px-1355px Исправлена работа слайдера

--- source/js/sliderScript.js
+++ source/js/sliderScript.js
@@ -7,13 +7,13 @@ const afterBtn = document.querySelector('#after-button');
 
 range.addEventListener('input',
   function() {
-    beforeContainer.style.clipPath = "polygon(0% 0%, 0% 100%, " + (100-this.value) +"% 100%, " + (100-this.value) +"% 0%)";
+    beforeContainer.style.clipPath = "polygon(100% 0%, 100% 100%, " + (0 + this.value) +"% 100%, " + (0 + this.value) +"% 0%)";
     afterContainer.style.width = this.value + "%";
   }, false);
 
 beforeBtn.addEventListener('click',
   function() {
-    beforeContainer.style.clipPath = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)";
+    beforeContainer.style.clipPath = "polygon(100% 0%, 100% 100%, 0% 100%, 0% 0%)";
     afterContainer.style.width = "0%";
     togglerMarker.style.left = "6px"
     togglerMarker.style.right = "50%";
@@ -22,7 +22,7 @@ beforeBtn.addEventListener('click',
 
 afterBtn.addEventListener('click',
   function() {
-    beforeContainer.style.clipPath = "polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)";
+    beforeContainer.style.clipPath = "polygon(100% 0%, 100% 100%, 100% 100%, 100% 0%)";
     afterContainer.style.width = "100%";
     togglerMarker.style.right = "6px";
     togglerMarker.style.left = "50%";
